7ab397a000da11001ac7290970701f5cb8845d49
mlgvla <mlgerardvla@gmail.com>
2020-12-29 17:17:04 -0500

    added User and Melody models and migrated to db

---
+++ db/migrate/20201229213032_create_melodies.rb
@@ -0,0 +1,12 @@
+class CreateMelodies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :melodies do |t|
+      t.string :title
+      t.text :notes
+      t.string :key
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
