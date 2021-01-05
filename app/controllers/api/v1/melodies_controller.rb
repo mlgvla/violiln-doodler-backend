5633a80b1e47fb05719fdb1bf017e19414534e7d
mlgvla <mlgerardvla@gmail.com>
2021-01-04 21:42:53 -0500

    added create action

--- app/controllers/api/v1/melodies_controller.rb
+++ app/controllers/api/v1/melodies_controller.rb
@@ -6,10 +6,22 @@ class Api::V1::MelodiesController < ApplicationController
     end
 
     def create
+        melody = Melody.new(melody_params)
+        if melody.save
+            render json: MelodySerializer.new(melody), status: :accepted
+          else
+            render json: { errors: melody.errors.full_messages }, status: :unprocessible_entity
+        end
     end
 
     def show
         melody = Melody.find(params[:id])
         render json: MelodySerializer.new(melody)
+    end
+
+    private
+
+    def melody_params
+        params.require(:melody).permit(:title, :notes, :key, :user_id)
     end
 end
