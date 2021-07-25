e643ff097ac72c7f4dcc0632e02c7c42e08c1039
mlgvla <mlgerardvla@gmail.com>
2021-07-25 01:37:58 -0400

    Added create action and user_params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,17 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
-        # to be implemented
+        user = User.new(user_params)
+        if user.save
+            render json: UserSerializer.new(user), status: :accepted
+          else
+            render json: { errors: user.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name)
     end
 end
